dcae4574b8b049ad3af812d3f69f86fedc04d6c2
Ritik Singh <hritikesingh2608@gmail.com>
2025-11-04 23:42:36 +0530

    Update projects.js

--- data/projects.js
+++ data/projects.js
@@ -1,174 +1,66 @@
-import { projects } from "../data/projects.js";
-import { clones } from "../data/projects.js";
+export const projects = [
+  {
+    video: "/video/steffers.com.mp4",
+    projectName: "RSNB.com",
+    type: "An E-COMMERCE Website",
+    summaryOne:
+      "Developed a fully functional e-commerce platform using React.js, featuring dynamic product listings and responsive design",
+    summaryTwo: "Implemented React Router for smooth navigation and routing.",
+    summaryThree:
+      "Integrated GSAP animations for interactive effects, for the mobile menu bar.",
+    summaryFour:
+      "Built a complete shopping cart system add/remove products, update cart, and auto-generate bills.",
+    projectLink: "https://react-project-three-olive-69.vercel.app/",
+  },
+  {
+    video: "/video/todo-app.mp4",
+    projectName: "TaskTrackrr Mobile To-do App",
+    type: "Todo - List",
+    summaryOne:
+      "Designed and built a mobile-friendly to-do list app using React.js, with a clean UI and smooth task management.",
+    summaryTwo:
+      "Implemented quick add & delete functionality using swipe gestures for an intuitive experience.",
+    summaryThree: "Added duedate & countdown timer support for each task.",
+    summaryFour:
+      "Integrated Framer Motion animations for seamless task add & remove transitions.",
+    projectLink: "https://react-todo-six-black.vercel.app/",
+  },
+  {
+    video: "/video/intelliQuiz.mp4",
+    projectName: "IntelliQuiz - Quiz app",
+    type: "Quiz App",
+    summaryOne:
+      " Developed a dynamic quiz application using React.js with category-based quizzes and a responsive layout.",
+    summaryTwo:
+      "Implemented React Router for seamless navigation across Quiz, Result, and Review pages.",
+    summaryThree:
+      "Utilized GSAP animations to create smooth transitions and engaging result effects.",
+    summaryFour:
+      "Designed a timer-based question system and score calculation logic.",
+    projectLink: "https://intelli-quiz-pi.vercel.app/",
+  },
+];
 
-let somethingHTML = "";
-
-projects.forEach((project) => {
-  somethingHTML += `
-    <div class="project">
-      <div class="project-video">
-        <video class="hover-this"
-          src="${project.video}"
-        ></video>
-      </div>
-      <div class="project-name">
-        <h2>${project.projectName}</h2>
-        <p>${project.type}</p>
-      </div>
-
-      <h2>Project Breakdown</h2>
-
-      <div class="features-container">
-        <ul>
-          <li>
-            ${project.summaryOne}
-          </li>
-          <li>
-            ${project.summaryTwo}
-          </li>
-          <li>
-            ${project.summaryThree}
-          </li>
-          <li>
-            ${project.summaryFour}
-          </li>
-        </ul>
-      </div>
-        <div class="arrow-box">
-          <a target="_blank" href="${project.projectLink}">
-            <div class="arrow-container">
-              <div class="arrow-link">
-                <img class="arrow-pic" src="/link svg.svg" alt="">
-                <img class="arrow-pic-2" src="/link svg.svg" alt="">
-              </div>
-            </div>
-          </a>
-        </div>
-    </div>
-  `;
-});
-
-document.querySelector(".js-all-projects").innerHTML = somethingHTML;
-
-let cloneProjectsHTML = "";
-
-clones.forEach((clone) => {
-  cloneProjectsHTML += `
-    <div class="each-project">
-      <video
-        class="hover-this"
-        loop
-        src="${clone.video}"
-      ></video>
-      <h2>${clone.site}</h2>
-      <p>${clone.content}</p>
-      <div class="arrow-box">
-        <a target="_blank" href="${clone.projectLink}">
-          <div class="arrow-container">
-            <div class="arrow-link">
-              <img class="arrow-pic" src="/link svg.svg" alt="">
-              <img class="arrow-pic-2" src="/link svg.svg" alt="">
-            </div>
-          </div>
-        </a>
-      </div>
-    </div>
-  `;
-});
-
-document.querySelector(".js-clone-projects").innerHTML = cloneProjectsHTML;
-
-document.querySelectorAll(".hover-this").forEach((video) => {
-  video.addEventListener("mouseover", () => {
-    video.play();
-
-    video.addEventListener("mouseout", () => {
-      video.pause();
-    });
-  });
-});
-
-const projectElem = document.querySelector(".js-projects-container");
-const projectMenu = document.querySelector(".menu-js-2");
-const navBar = document.querySelector(".js-navbar");
-const developerElem = document.querySelector(".js-developer-detail");
-const contactElem = document.querySelector(".js-contact-container");
-const aboutElem = document.querySelector(".js-about-page");
-const cloneElem = document.querySelector(".js-clone-sites");
-
-projectMenu.addEventListener("click", () => {
-  developerElem.style.display = "none";
-  contactElem.style.display = "none";
-  cloneElem.style.display = "none";
-  projectElem.style.display = "block";
-  aboutElem.style.display = "none";
-
-  hideAndroidMenus();
-});
-
-const homeMenu = document.querySelector(".menu-js-1");
-
-homeMenu.addEventListener("click", () => {
-  developerElem.style.display = "block";
-  contactElem.style.display = "none";
-  projectElem.style.display = "none";
-  cloneElem.style.display = "none";
-  aboutElem.style.display = "none";
-
-  hideAndroidMenus();
-});
-
-const contactMe = document.querySelector(".js-contact");
-
-contactMe.addEventListener("click", () => {
-  developerElem.style.display = "none";
-  projectElem.style.display = "none";
-  cloneElem.style.display = "none";
-  contactElem.style.display = "block";
-  aboutElem.style.display = "none";
-
-  hideAndroidMenus();
-});
-
-const aboutMenu = document.querySelector(".menu-js-4");
-
-aboutMenu.addEventListener("click", () => {
-  developerElem.style.display = "none";
-  projectElem.style.display = "none";
-  contactElem.style.display = "none";
-  cloneElem.style.display = "none";
-
-  aboutElem.style.display = "block";
-
-  hideAndroidMenus();
-});
-
-const cloneMenu = document.querySelector(".menu-js-3");
-cloneMenu.addEventListener("click", () => {
-  developerElem.style.display = "none";
-  projectElem.style.display = "none";
-  contactElem.style.display = "none";
-  aboutElem.style.display = "none";
-
-  cloneElem.style.display = "block";
-
-  hideAndroidMenus();
-});
-
-const androidMenu = document.querySelector(".hamburger-menu");
-
-const androidHeader = document.querySelector(".androidHeaderToo");
-
-androidMenu.addEventListener("click", () => {
-  if (androidHeader.style.display === "flex") {
-    androidHeader.style.display = "none";
-  } else {
-    androidHeader.style.display = "flex";
-  }
-});
-
-function hideAndroidMenus() {
-  if (window.innerWidth <= 450) {
-    androidHeader.style.display = "none";
-  }
-}
+export const clones = [
+  {
+    video: "/video/flipkart.com.mp4",
+    site: "Flipkart",
+    content:
+      "Created a Flipkart clone using HTML and CSS, replicating the homepage layout and design of the original website. It includes a navigation bar, product listings, banners, and a styled footer. This project helped me strengthen my HTML and CSS skills by recreating a real-world e-commerce UI.",
+    projectLink: "./youtube-clone/youtube.html",
+  },
+  {
+    video: "/video/YouTube.com.mp4",
+    site: "Youtube",
+    content:
+      "Made a YouTube homepage clone using HTML and CSS. It has a top bar with a logo and search box, a side menu, and video boxes with titles and channel names. This project helped me get better at using HTML and CSS to build static and well-structured website layouts.",
+    projectLink: "../youtube-clone/youtube-clone/youtube.html",
+  },
+  {
+    video: "/video/Google.com.mp4",
+    site: "Google.com",
+    content:
+      "Created a Google Search homepage clone using HTML and CSS. It includes the Google logo, a search box, and buttons like the real site. This project helped me practice how to center elements and style a clean, simple layout using basic HTML and CSS.",
+    projectLink: "../youtube-clone/Reuters/reuters.html",
+  },
+];
